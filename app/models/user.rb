b745dee4ad41f744424ce478519b8c304ff49cf1
Steve Loveless <steve.loveless@gmail.com>
2013-09-19 01:38:02 -0700

    remember_token model and DB changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,25 @@ class User < ActiveRecord::Base
     self.username = username.downcase
   end
 
+  before_create :create_remember_token
+
   has_many :photos, dependent: :destroy
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
