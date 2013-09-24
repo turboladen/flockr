f0ba16ce2796ea8f779f48c8952b9ddfd7f9d877
Steve Loveless <steve.loveless@gmail.com>
2013-09-23 23:40:52 -0700

    Added validation on Photo#file_name

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -4,9 +4,20 @@ describe Photo do
   it { should respond_to :user }
   it { should respond_to :image }
 
+  subject do
+    Photo.new(file_name: 'test.file')
+  end
+
+  describe 'file_name' do
+    it 'is required' do
+      expect(Photo.new.errors_on(:file_name)).to include "can't be blank"
+    end
+  end
+
   it 'can belong to a user' do
-    user = User.new
-    photo = Photo.new(user: user)
-    photo.should be_valid
+    user = User.new(username: 'test', email: 'test@test.com', password: 'password',
+      password_confirmation: 'password')
+    subject.user = user
+    subject.should be_valid
   end
 end
