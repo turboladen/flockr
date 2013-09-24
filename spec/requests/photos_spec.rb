20e7c35e53a86e4c2376835e84b4eb1e7808295a
Steve Loveless <steve.loveless@gmail.com>
2013-09-23 20:17:07 -0700

    Fixed formatting/style
    Mostly replaced double quotes with singles where appropriate and
    converted hash rockets (=>) to javascript style hashes (key: ).

--- spec/requests/photos_spec.rb
+++ spec/requests/photos_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe "Photos" do
-  describe "GET /photos" do
-    it "works! (now write some real specs)" do
+describe 'Photos' do
+  describe 'GET /photos' do
+    it 'works! (now write some real specs)' do
       user = User.first || User.create(email: 'test@test.com', username: 'test')
 
       get user_photos_path(user)
