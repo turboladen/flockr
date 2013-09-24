969ee7f409f8058cfdcc0c753cd22de6b7094908
Steve Loveless <steve.loveless@gmail.com>
2013-09-23 20:36:40 -0700

    Fixed request spec

--- spec/requests/photos_spec.rb
+++ spec/requests/photos_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'Photos' do
   describe 'GET /photos' do
     it 'works! (now write some real specs)' do
-      user = User.first || User.create(email: 'test@test.com', username: 'test')
+      user = User.first || User.create(email: 'test@test.com', username: 'test',
+        password: 'password', password_confirmation: 'password')
 
       get user_photos_path(user)
       response.status.should be(200)
